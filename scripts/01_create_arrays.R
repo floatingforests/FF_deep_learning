a20775f31406856c7c269adbf00e177a13c5a6ff
Irosenthal <irosenthal121@gmail.com>
2020-11-10 17:34:55 -0500

    fixed tensor generation function

--- scripts/01_create_arrays.R
+++ scripts/01_create_arrays.R
@@ -1,3 +1,6 @@
+#code to import floating forests swipe data and prepare it for keras 
+#data was generated from binary yes/no classifications made by citizen scientists on zooniverse.com
+
 #libraries
 library(tidyverse)
 library(raster)
@@ -24,6 +27,10 @@ falk_with_meta <- falk_with_meta %>%
 falk_labels <- falk_with_meta %>%
   dplyr::select(subject_id, fraction_yes)
 
+##################
+#Helper functions
+##################
+
 #function to download, rasterize, and convert ff images to arrays
 array_from_subject <- function(subject){
   subj_metadata <- subjects %>% filter(subject_id == subject)
@@ -41,13 +48,31 @@ array_from_subject <- function(subject){
   img@extent = raster::extent(as.numeric(subj_metadata$`#tile_UL_x`), as.numeric(subj_metadata$`#tile_LR_x`),
                               as.numeric(subj_metadata$`#tile_LR_y`), as.numeric(subj_metadata$`#tile_UL_y`))
   
-  return(as.array(img))
+  ff_array <- as.array(img)
+  
   unlink(filename)
   
+  
+  return(ff_array)
+
 }
 
-
-#i
+#function that takes a bunch of subjects and creates array_unlisted from those subjects
+generate_input_tensors <- function(sub_vector){
+  subject_test_set <- clean_subjects %>%
+    filter(subject_id %in% sub_vector)
+  
+  #download all images and convert to array
+  array_list <- map(subject_test_set$subject_id, array_from_subject)
+  #convert this list of 3d arrays to a 4d array
+  array_unlisted <- aperm(array(as.numeric(unlist(array_list)), 
+                                dim=c(500, 500, 3, length(array_list))), 
+                          c(4, 1, 2, 3))
+  
+  return(array_unlisted)
+}
+###############################
+#our goals:
 #input: subject id
 #output: 500x500x3 array 
 
@@ -59,6 +84,8 @@ dim(array_test)
 
 
 ##################################
+#Time out: not all subjects are 500x500
+
 #how many subjects are the wrong size?
 bad_subjects <- falk_with_meta %>%
   filter(`#width` != 350 |
@@ -77,14 +104,6 @@ bad_subjects <- falk_with_meta %>%
 #https://panoptes-uploads.zooniverse.org/production/subject_location/23f70d98-90b2-4272-842f-069a982d71ae.jpeg	
 #drop these for now?
 
-
-
-#double check on older subject data
-#only for troubleshooting - can ignore
-#sub_relaunch_master <- readRDS("../../FF/clean/data/relaunch_data/level_0/ff_relaunch_subjects.rds")
-#it exists but is missing a lot of data
-#missing_sub_check <- sub_relaunch_master %>% filter(subject_id == 2934266)
-
 ######################
 
 #for now, drop all that are NA or the wrong size
@@ -95,80 +114,14 @@ clean_subjects <- falk_with_meta %>%
            !is.na(`#width`) &
            !is.na(`#height`)) 
 
-
-
-#this small subset will ultimately be the training data
-#4 subjects for now so that our computers don't melt
-subs_of_interest <- c(28751352,
-                      28755524,
-                      28754383,
-                      28756527)
-
-subject_test_set <- clean_subjects %>%
-  filter(subject_id %in% subs_of_interest)
-
-
-#download all images and convert to array
-array_list <- map(subject_test_set$subject_id, array_from_subject)
-
-#convert this list of 3d arrays to a 4d array
-array_unlisted <- aperm(array(as.numeric(unlist(array_list)), 
-                    dim=c(500, 500, 3, length(array_list))), 
-                    c(length(array_list), 1, 2, 3))
-
-
-#create training and test data with labels
-
-#function that takes a bunch of subjects and creates array_unlisted from those subjects
-generate_input_tensors <- function(sub_vector){
-  subject_test_set <- clean_subjects %>%
-    filter(subject_id %in% sub_vector)
-  
-  #download all images and convert to array
-  array_list <- map(subject_test_set$subject_id, array_from_subject)
-  #convert this list of 3d arrays to a 4d array
-  array_unlisted <- aperm(array(as.numeric(unlist(array_list)), 
-                                dim=c(500, 500, 3, length(array_list))), 
-                          c(4, 1, 2, 3))
-  
-  return(array_unlisted)
-}
-
-
 #generate training/testing data/labels
 ff_train <- generate_input_tensors(clean_subjects$subject_id[1:10])
 ff_test <- generate_input_tensors(clean_subjects$subject_id[11:20])
 ff_train_labels <- falk_labels[1:10,]
 ff_test_labels <- falk_labels[11:20,]
 
+#save as a list for easy loading
 saveRDS(list(ff_train = ff_train, 
              ff_test = ff_test, 
              ff_train_labels = ff_train_labels,
              ff_test_labels = ff_test_labels), "../data/ff_tensor_data.RDS")
-
-
-ff_data <- readRDS("../data/ff_tensor_data.RDS")
-#need to write code to separate train/test/labels
-
-
-model <- keras_model_sequential() %>%
-  layer_dense(units = 16, activation = "relu", input_shape = c(10)) %>%
-  layer_dense(units = 16, activation = "relu") %>%
-  layer_dense(units = 1, activation = "sigmoid")
-
-model %>% compile(
-  optimizer = "rmsprop",
-  loss = "binary_crossentropy",
-  metrics = c("accuracy")
-)
-
-history <- model %>% fit(
-  x = ff_train,
-  y = as.vector(ff_train_labels$fraction_yes),
-  epochs = 20,
-  batch_size = 10,
-  validation_data = list(ff_test, as.vector(ff_test_labels$fraction_yes))
-)
-
-
-#split data generation and model fitting in 
--- scripts/02_fit_models.R
+++ scripts/02_fit_models.R
@@ -1,20 +1,30 @@
+#code to fit a model with keras
+#given a set of 3D raster stacks, what is the probability that each contains kelp?
+
+
+#libraries
 library(tidyverse)
 library(keras)
+
+#load data - this is a list containing test and training data with labels
+
 ff_data <- readRDS("../data/ff_tensor_data.RDS")
 #need to write code to separate train/test/labels
 
-
+#set shape to size of training data?
 model <- keras_model_sequential() %>%
-  layer_dense(units = 16, activation = "relu", input_shape = c(10)) %>%
+  layer_dense(units = 16, activation = "relu", input_shape = c(3)) %>%
   layer_dense(units = 16, activation = "relu") %>%
   layer_dense(units = 1, activation = "sigmoid")
 
+#directly from textbook
 model %>% compile(
   optimizer = "rmsprop",
   loss = "binary_crossentropy",
   metrics = c("accuracy")
 )
 
+#set batch size to 10 - should it be lower than the number of input tensors?
 history <- model %>% fit(
   x = ff_train,
   y = as.vector(ff_train_labels$fraction_yes),
