5cce3e0fc87ab1798129c3bf6a7fe19c1f0456ad
jebyrnes <jarrett.byrnes@umb.edu>
2021-01-09 14:13:24 -0500

    Add kellymod

---
+++ scripts/kellymod/00_constants.R
@@ -0,0 +1,5 @@
+# Set up Project Parent Data Directory
+# Set Working Directory for My Sanity
+working_dir = here::here()
+project_dirname <- 'mod_a'
+dat_path <- paste0(working_dir, '/data/', project_dirname)
---
+++ scripts/kellymod/02_image_translate.R
@@ -0,0 +1,36 @@
+library(raster)
+library(keras)
+library(tensorflow)
+
+# Set Working Directory for My Sanity
+working_dir <- here::here()
+script_dir <- paste0(working_dir, "/scripts/kellymod/")
+
+#load constants
+source(paste0(script_dir, "00_constants.R"))
+
+
+# Set up Directories
+train_dir = file.path(dat_path, 'train')
+test_dir = file.path(dat_path, 'test')
+val_dir = file.path(dat_path, 'validation')
+
+# Set Up Image Generator
+datagen = image_data_generator(featurewise_center = TRUE,
+                               featurewise_std_normalization = TRUE)
+
+train_generator = flow_images_from_directory(
+  train_dir,
+  datagen,
+  target_size = c(500,500),
+  batch_size = 20,
+  class_mode = "binary"
+)
+
+valid_generator = flow_images_from_directory(
+  val_dir,
+  datagen,
+  target_size = c(500,500),
+  batch_size = 20,
+  class_mode = "binary"
+)
